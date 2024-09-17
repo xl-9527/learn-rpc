019c816fe72c3a2be108129e00c07f799a29f1e5
XingGuangLei <1824670705@qq.com>
2024-09-17 22:40:42 +0800

    feat: init

---
+++ src/main/java/com/rpc/RpcDemo.java
@@ -0,0 +1,12 @@
+package com.rpc;
+
+/**
+ * @author xl-9527
+ * @since 2024/9/17
+ **/
+public class RpcDemo {
+
+    public static void main(String[] args) {
+        System.out.println("RPC-DEMO");
+    }
+}
