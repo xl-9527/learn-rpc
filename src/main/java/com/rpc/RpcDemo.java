07566c2344931eb4c4d4d9b7faf99b333d840bd7
XingGuangLei <1824670705@qq.com>
2024-09-17 23:18:10 +0800

    feta：引入日志系统

--- src/main/java/com/rpc/RpcDemo.java
+++ src/main/java/com/rpc/RpcDemo.java
@@ -1,12 +1,17 @@
 package com.rpc;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * @author xl-9527
  * @since 2024/9/17
  **/
 public class RpcDemo {
 
+    private static final Logger log = LoggerFactory.getLogger(RpcDemo.class);
+
     public static void main(String[] args) {
-        System.out.println("RPC-DEMO");
+        log.info("RPC-DEMO");
     }
 }
