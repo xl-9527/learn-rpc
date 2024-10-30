eb569ae044354c326c3e091b2b2a467f5e399fe2
xl-9527 <1824670705@qq.com>
2024-10-30 22:43:48 +0800

    feat(server): 添加线程池服务器实现
    - 在 TestServer 类中添加了 tThreadPoolServer 方法，使用 TThreadPoolServer 替代 TSimpleServer
    - 新增线程池服务器的配置和启动逻辑
    - 保留了原有的简单服务器实现，改为 tSimpleServer 方法

--- thrift-rpc/idl/study_idl/gen.bat
+++ thrift-rpc/idl/study_idl/gen.bat
@@ -4,5 +4,5 @@ thrift --gen java test.thrift
 thrift -help
 @REM -o dir      Set the output directory for gen-* packages
 
+@REM -r 表示可以在引用文件时，引用文件也生成代码（include 对应的引入
 thrift -r --gen java
-
--- thrift-rpc/thrift-rpc-server/src/main/java/com/rpc/TestServer.java
+++ thrift-rpc/thrift-rpc-server/src/main/java/com/rpc/TestServer.java
@@ -4,6 +4,7 @@ import com.rpc.service.UserServiceImpl;
 import com.rpc.thrift.UserService;
 import org.apache.thrift.protocol.TBinaryProtocol;
 import org.apache.thrift.server.TSimpleServer;
+import org.apache.thrift.server.TThreadPoolServer;
 import org.apache.thrift.transport.TServerSocket;
 import org.apache.thrift.transport.TServerTransport;
 import org.apache.thrift.transport.TTransportException;
@@ -15,6 +16,24 @@ import org.apache.thrift.transport.TTransportException;
 public class TestServer {
 
     public static void main(String[] args) throws TTransportException {
+        // tSimpleServer();
+        tThreadPoolServer();
+    }
+
+    private static void tThreadPoolServer() throws TTransportException {
+        // TTransportFactory
+        try (final TServerTransport serverTransport = new TServerSocket(9090)) {
+            final TThreadPoolServer threadPoolServer = new TThreadPoolServer(
+                    new TThreadPoolServer.Args(serverTransport)
+                            .protocolFactory(new TBinaryProtocol.Factory())
+                            .processor(new UserService.Processor<>(new UserServiceImpl()))
+            );
+
+            threadPoolServer.serve();
+        }
+    }
+
+    private static void tSimpleServer() throws TTransportException {
         try (TServerTransport transport = new TServerSocket(9090)) {
             final UserService.Processor<UserService.Iface> processor = new UserService.Processor<>(new UserServiceImpl());
             final TSimpleServer server = new TSimpleServer(new TSimpleServer.Args(transport)
