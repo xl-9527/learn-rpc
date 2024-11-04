5f27f7e71be69d615f60a9232889976100fd7e10
xl-9527 <1824670705@qq.com>
2024-11-04 23:12:38 +0800

    feat(rpc): 添加 gRPC模块初始化
    - 新建 gRPC 模块目录和基本文件结构
    - 创建 grpc-study.proto 文件，定义协议缓冲区
    - 添加 Maven 项目对象模型 (POM) 文件配置
    - 在根 POM 文件中引用 gRPC模块
    - 编写简单的 GrpcRpcMain 类作为模块入口

---
+++ grpc-rpc/src/main/java/com/rpc/grpc/GrpcRpcMain.java
@@ -0,0 +1,11 @@
+package com.rpc.grpc;
+
+/**
+ * @author xl-9527
+ * @since 2024/11/4
+ **/
+public class GrpcRpcMain {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
