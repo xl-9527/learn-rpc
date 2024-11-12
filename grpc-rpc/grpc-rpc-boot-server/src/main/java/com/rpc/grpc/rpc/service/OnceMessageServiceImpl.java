10d8f9a76d4346eb010c195c1867f94772c0aa99
xl-9527 <1824670705@qq.com>
2024-11-13 00:02:51 +0800

    feat(rpc): 添加一次性消息服务实现
    - 新增 OnceMessageServiceImpl 类实现了一次性消息服务- 添加 grpc-api 依赖
    - 服务接收客户端请求消息，并返回一个随机生成的 UUID 作为响应

---
+++ grpc-rpc/grpc-rpc-boot-server/src/main/java/com/rpc/grpc/rpc/service/OnceMessageServiceImpl.java
@@ -0,0 +1,29 @@
+package com.rpc.grpc.rpc.service;
+
+import com.rpc.grpc.bi.BiRpcServiceGrpc;
+import com.rpc.grpc.bi.BiService;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.UUID;
+
+/**
+ * @author xl-9527
+ * @since 2024/11/12
+ **/
+@GrpcService
+public class OnceMessageServiceImpl extends BiRpcServiceGrpc.BiRpcServiceImplBase {
+
+    private final Logger log = LoggerFactory.getLogger(OnceMessageServiceImpl.class);
+
+    @Override
+    public void getOnceMessage(final BiService.OnceMessageRequest request, final StreamObserver<BiService.OnceMessageResponse> responseObserver) {
+        log.info("receive client request msg -> {}", request.getContent());
+        responseObserver.onNext(
+                BiService.OnceMessageResponse.newBuilder().setContent(UUID.randomUUID().toString()).build()
+        );
+        responseObserver.onCompleted();
+    }
+}
