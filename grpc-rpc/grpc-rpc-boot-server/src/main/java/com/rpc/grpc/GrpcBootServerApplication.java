ef4504f53fc456d36d9d38568c01f32b53810791
xl-9527 <1824670705@qq.com>
2024-11-12 23:43:52 +0800

    feat(grpc-rpc-boot-server): 添加 gRPC 服务器模块并集成 Spring Boot
    - 新增 grpc-rpc-boot-server 模块，使用 Spring Boot 框架
    - 配置 Maven 项目对象模型 (pom.xml) 文件
    - 添加必要的依赖项，包括 Spring Boot 和 gRPC 服务器启动器- 创建 GrpcBootServerApplication 类作为应用程序入口

---
+++ grpc-rpc/grpc-rpc-boot-server/src/main/java/com/rpc/grpc/GrpcBootServerApplication.java
@@ -0,0 +1,15 @@
+package com.rpc.grpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xl-9527
+ * @since 2024/11/12
+ **/
+@SpringBootApplication
+public class GrpcBootServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcBootServerApplication.class, args);
+    }
+}
