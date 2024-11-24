02afa627407d0d8cb9f77192567fa420d1cfeb05
xl-9527 <1824670705@qq.com>
2024-11-25 07:43:57 +0800

    feat(grpc-server): 整合 Consul 服务发现与心跳监测
    - 在 application.yaml 中添加 Consul 配置，启用服务发现和心跳功能
    - 在 GrpcBootServerApplication 中添加 @EnableDiscoveryClient 注解
    - 更新 pom.xml，移除不必要的 exclusions，并添加 Actuator 和 Consul 发现依赖

--- grpc-rpc/grpc-rpc-boot-server/src/main/java/com/rpc/grpc/GrpcBootServerApplication.java
+++ grpc-rpc/grpc-rpc-boot-server/src/main/java/com/rpc/grpc/GrpcBootServerApplication.java
@@ -2,11 +2,13 @@ package com.rpc.grpc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author xl-9527
  * @since 2024/11/12
  **/
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GrpcBootServerApplication {
     public static void main(String[] args) {
