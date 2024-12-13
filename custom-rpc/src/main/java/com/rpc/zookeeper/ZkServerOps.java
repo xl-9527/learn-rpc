8e9e2a7fa00b3835a9ea32d7c0f18bb55a9ca0ca
xl-9527 <1824670705@qq.com>
2024-12-13 22:52:59 +0800

    feat(zookeeper): 重构 ZkServerOps 类并添加新功能- 重构 createNode 方法，支持创建带数据的节点 - 新增 deleteNode、setValue、watchNode 等方法 - 优化 exists 方法，提高代码可读性 - 在测试类中添加相关测试用例

--- custom-rpc/src/main/java/com/rpc/zookeeper/ZkServerOps.java
+++ custom-rpc/src/main/java/com/rpc/zookeeper/ZkServerOps.java
@@ -2,8 +2,12 @@ package com.rpc.zookeeper;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.zookeeper.*;
+import org.apache.zookeeper.data.ACL;
+import org.apache.zookeeper.data.Stat;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * @author xl-9527
@@ -23,34 +27,76 @@ public class ZkServerOps {
     }
 
     public String createNode(String path) {
+        return this.createNode(path, null, ZooDefs.Ids.OPEN_ACL_UNSAFE);
+    }
+
+    public String creatDataNode(String path, byte[] bytes) {
+        return this.createNode(path, bytes, ZooDefs.Ids.OPEN_ACL_UNSAFE);
+    }
+
+    public String createNode(String path, byte[] bytes, List<ACL> acl) {
         try {
-            return zooKeeper.create(path, null, null, CreateMode.PERSISTENT);
+            if (!Objects.isNull(bytes) && this.exists(path)) {
+                throw new RuntimeException("数据节点不可以对已经存在的节点操作");
+            }
+            return zooKeeper.create(path, bytes, acl, CreateMode.PERSISTENT);
         } catch (KeeperException | InterruptedException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public void deleteNode() {
+    public void deleteNode(String path) {
+        try {
+            zooKeeper.delete(path, -1);
+        } catch (InterruptedException | KeeperException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public void deleteAllNode() {
+    public Stat setValue(String path, byte[] bytes) {
+        try {
+            if (!exists(path)) {
+                return zooKeeper.setData(path, bytes, -1);
+            }
+
+            return null;
+        } catch (KeeperException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public void setValue() {
+    public void watchNode(String path) {
+        try {
+            zooKeeper.addWatch(path, event -> log.info("receive watch event -> {}, and is ok", event), AddWatchMode.PERSISTENT_RECURSIVE);
+        } catch (KeeperException | InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                System.in.read();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
-    public void watchNode() {
+    public List<String> getNode(String path) {
+        try {
+            return zooKeeper.getChildren(path, false);
+        } catch (KeeperException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public void getNode() {
+    public boolean exists(String path) {
+        try {
+            final Stat exists = zooKeeper.exists(path, false);
+            return exists != null;
+        } catch (KeeperException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public ZooKeeper initServer() throws IOException {
-        return new ZooKeeper("101.35.52.157:2181", 10000, new Watcher() {
-            @Override
-            public void process(final WatchedEvent watchedEvent) {
-                log.info("receive watch event -> {}", watchedEvent);
-            }
-        });
+        return new ZooKeeper("101.35.52.157:2181", 10000, watchedEvent -> log.info("receive watch event -> {}", watchedEvent));
     }
 }
--- custom-rpc/src/test/java/com/rpc/zookeeper/ZkServerOpsTest.java
+++ custom-rpc/src/test/java/com/rpc/zookeeper/ZkServerOpsTest.java
@@ -1,14 +1,12 @@
 package com.rpc.zookeeper;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.data.Stat;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author xl-9527
@@ -25,6 +23,50 @@ class ZkServerOpsTest {
             log.info("链接成功 zk 服务 -> {}", zooKeeper);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void createNode() {
+        final String node = zkServerOps.createNode("/test/watch-t");
+        log.info("创建节点 -> {}", node);
+    }
+
+    @Test
+    void deleteNode() {
+        zkServerOps.deleteNode("/test/watch-t");
+    }
+
+    @Test
+    void setValue() {
+        final Stat stat = zkServerOps.setValue("/zookeeper-val", "hello-1".getBytes());
+        log.info("修改值 -> {}", stat);
+    }
+
+    @Test
+    void creatDataNode() {
+        final String dataNode = zkServerOps.creatDataNode("/zookeeper-valsahdksalkdkjsad", "hello".getBytes());
+        log.info("创建数据节点 -> {}", dataNode);
+    }
+
+    @Test
+    void exists() {
+        final boolean existsNot = zkServerOps.exists("/zookeeper-val-not");
+        final boolean exists = zkServerOps.exists("/zookeeper-val");
+        log.info("节点是否存在 -> {}", exists);
+        log.info("节点是否存在 -> {}", existsNot);
+    }
+
+    @Test
+    void watchNode() {
+        zkServerOps.watchNode("/test/watch-t");
+    }
+
+    @Test
+    void getNode() {
+        final List<String> node = zkServerOps.getNode("/");
+        for (final String nodePath : node) {
+            log.info("获取节点 -> {}", nodePath);
         }
     }
 }
