c5517229e2cbbbfaf089854ad16dc4989cf96583
xl-9527 <1824670705@qq.com>
2024-12-24 22:45:58 +0800

    test:移除 StubTest 中未使用的导入语句
    移除了 StubTest.java 文件中未使用的 org.junit.jupiter.api.Assertions 导入语句。这个修改提高了代码的可读性和维护性，避免了不必要的

--- custom-rpc/src/test/java/com/custom_rpc/proxy/StubTest.java
+++ custom-rpc/src/test/java/com/custom_rpc/proxy/StubTest.java
@@ -3,8 +3,6 @@ package com.custom_rpc.proxy;
 import com.custom_rpc.service.HelloService;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 /**
  * @author xl-9527
  * @since 2024/12/24
