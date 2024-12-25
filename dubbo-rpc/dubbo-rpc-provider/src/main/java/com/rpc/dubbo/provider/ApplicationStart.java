5da6d9453e05ca0323883fcd3d089303d4622100
xl-9527 <1824670705@qq.com>
2024-12-25 09:34:30 +0800

    feat(dubbo-rpc-provider): 添加应用启动时的Bean定义名称日志输出
    - 在 ApplicationStart 类中添加了日志输出，列出了所有Bean定义名称
    - 修改了 application-context.xml 的加载路径，去除了 classpath*:
    -优化了代码结构，增加了日志的可读性和维护性

--- dubbo-rpc/dubbo-rpc-provider/src/main/java/com/rpc/dubbo/provider/ApplicationStart.java
+++ dubbo-rpc/dubbo-rpc-provider/src/main/java/com/rpc/dubbo/provider/ApplicationStart.java
@@ -1,5 +1,6 @@
 package com.rpc.dubbo.provider;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.concurrent.CountDownLatch;
@@ -8,12 +9,17 @@ import java.util.concurrent.CountDownLatch;
  * @author xl-9527
  * @since 2024/12/24
  **/
+@Slf4j
 public class ApplicationStart {
 
     public void start() throws InterruptedException {
-        final ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("classpath*:application-context.xml");
+        final ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("application-context.xml");
         ctx.start();
-
+        log.info("------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+        for (final String definitionName : ctx.getBeanDefinitionNames()) {
+            log.info("|-----{}", definitionName);
+        }
+        log.info("------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
         new CountDownLatch(1).await();
     }
 }
