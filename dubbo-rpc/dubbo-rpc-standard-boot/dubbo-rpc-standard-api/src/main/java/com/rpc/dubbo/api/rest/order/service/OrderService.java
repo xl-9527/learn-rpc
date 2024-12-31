749ed5559a34b55839091b353451f6f0b7d68707
xl-9527 <1824670705@qq.com>
2024-12-31 17:54:33 +0800

    feat(rpc): 更新 Dubbo RESTful RPC 服务示例
    - 修改应用配置，添加 Zookeeper 注册中心
    - 更新 OrderService接口，添加 RESTful 注解
    - 调整 OrderServiceImpl 实现类
    - 更新 Maven依赖：
      - Spring Boot 升级到 3.4.1
      - Dubbo 升级到 3.2.14
      - gRPC 升级到 1.69.0
      - logback 升级到 1.5.12
      - slf4j升级到 2.0.16
      - 删除 java.version 属性
      - 添加 commons-io 和 okio 依赖（替换之前有漏洞的 dependence）

--- dubbo-rpc/dubbo-rpc-standard-boot/dubbo-rpc-standard-api/src/main/java/com/rpc/dubbo/api/rest/order/service/OrderService.java
+++ dubbo-rpc/dubbo-rpc-standard-boot/dubbo-rpc-standard-api/src/main/java/com/rpc/dubbo/api/rest/order/service/OrderService.java
@@ -1,12 +1,18 @@
 package com.rpc.dubbo.api.rest.order.service;
 
 import com.rpc.dubbo.api.rest.order.Order;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author xl-9527
  * @since 2024/12/31
  **/
+@RestController
+@RequestMapping("order")
 public interface OrderService {
 
+    @GetMapping
     Order getOrderById(Long orderId);
 }
--- dubbo-rpc/dubbo-rpc-standard-boot/dubbo-rpc-standard-boot-provider/src/main/java/com/rpc/dubbo/provider/rest/order/service/impl/OrderServiceImpl.java
+++ dubbo-rpc/dubbo-rpc-standard-boot/dubbo-rpc-standard-boot-provider/src/main/java/com/rpc/dubbo/provider/rest/order/service/impl/OrderServiceImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
  * @since 2024/12/31
  **/
 @Service
-@DubboService(protocol = "rest")
+@DubboService
 public class OrderServiceImpl implements OrderService {
 
     @Override
