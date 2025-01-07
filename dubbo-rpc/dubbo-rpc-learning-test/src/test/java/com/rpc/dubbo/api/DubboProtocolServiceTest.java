d22c03bf49cbdc50661086d080473eefe9957806
xl-9527 <1824670705@qq.com>
2025-01-07 17:36:10 +0800

    test(dubbo-rpc): 添加 Dubbo 协议服务测试用例
    - 新增 application-test.yml配置文件，用于测试环境配置
    - 修改 DubboProtocolServiceTest

--- dubbo-rpc/dubbo-rpc-learning-test/src/test/java/com/rpc/dubbo/api/DubboProtocolServiceTest.java
+++ dubbo-rpc/dubbo-rpc-learning-test/src/test/java/com/rpc/dubbo/api/DubboProtocolServiceTest.java
@@ -1,8 +1,12 @@
 package com.rpc.dubbo.api;
 
 import org.apache.dubbo.config.annotation.DubboReference;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,13 +15,15 @@ import static org.junit.jupiter.api.Assertions.*;
  * @since 2025/1/5
  **/
 @SpringBootTest
+@ActiveProfiles("test")
 class DubboProtocolServiceTest {
 
+    private static final Logger log = LoggerFactory.getLogger(DubboProtocolServiceTest.class);
     @DubboReference(protocol = "dubbo")
     private DubboProtocolService dubboProtocolService;
 
-    @Test
+    @RepeatedTest(5)
     void test() {
-        System.out.println("dubboProtocolService.test(\"hello\") = " + dubboProtocolService.test("hello"));
+        log.info("dubboProtocolService.test(\"hello\") = {}", dubboProtocolService.test("hello"));
     }
 }
