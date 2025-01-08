1092b48e22b276dac94aa7a6eec8220c2312bc62
xl-9527 <1824670705@qq.com>
2025-01-08 11:25:43 +0800

    refactor(dubbo-rpc-learning-test): 移除 RESTful 协议配置并升级 Dubbo 版本
    - 删除了 application.yml 中的 restful 协议配置- 将 Dubbo 版本从 3.3.0升级到 3.3.1
    - 移除了 RestfulProtocolServiceImpl 类中的 @DubboService(protocol = "rest") 注解- 添加了 dubbo-remoting-http 依赖

--- dubbo-rpc/dubbo-rpc-learning-test/src/main/java/com/rpc/dubbo/service/RestfulProtocolServiceImpl.java
+++ dubbo-rpc/dubbo-rpc-learning-test/src/main/java/com/rpc/dubbo/service/RestfulProtocolServiceImpl.java
@@ -9,7 +9,7 @@ import java.util.Map;
  * @author xl-9527
  * @since 2025/1/5
  **/
-@DubboService(protocol = "rest")
+@DubboService
 public class RestfulProtocolServiceImpl implements RestfulProtocolService {
 
     @Override
