259fbb85f8b41b3427785c7374aa52296150c5ca
xl-9527 <1824670705@qq.com>
2025-01-11 18:24:36 +0800

    feat(provider): 添加自定义提供者过滤器
    - 在 dubbo-rpc-standard-boot-provider 模块中添加 MyProviderFilter 类
    - 使用 @Activate 注解指定过滤器运行在提供者端
    - 实现 Filter 接口，重写 invoke 方法添加日志输出
    - 在 META-INF/dubbo/org.apache.dubbo.rpc.Filter 文件中注册过滤器

--- dubbo-rpc/dubbo-rpc-standard-boot/dubbo-rpc-standard-boot-provider/src/main/java/com/rpc/dubbo/provider/filter/MyProviderFilter.java
+++ dubbo-rpc/dubbo-rpc-standard-boot/dubbo-rpc-standard-boot-provider/src/main/java/com/rpc/dubbo/provider/filter/MyProviderFilter.java
@@ -1,6 +1,8 @@
 package com.rpc.dubbo.provider.filter;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.dubbo.common.constants.CommonConstants;
+import org.apache.dubbo.common.extension.Activate;
 import org.apache.dubbo.rpc.*;
 
 /**
@@ -8,14 +10,19 @@ import org.apache.dubbo.rpc.*;
  * @since 2025/1/11
  **/
 @Slf4j
+@Activate(group = CommonConstants.PROVIDER) // active 中 group 的设置是表示他运行在 consumer 还是 provider 端，这是必须要要配置的
 public class MyProviderFilter implements Filter {
 
     /**
      * @param invoker    实际这次的调用
      * @param invocation 本次调用的参数
+     * @return 调用结果，这里的调用结果是从 provider 端调用过来的
      */
     @Override
     public Result invoke(final Invoker<?> invoker, final Invocation invocation) throws RpcException {
+        System.out.println("invoker.getUrl() = " + invoker.getUrl());
+        System.out.println("invocation.getMethodName() = " + invocation.getMethodName());
+        System.out.println("invocation.getServiceModel().getServiceName() = " + invocation.getServiceModel().getServiceName());
         log.info("MyProviderFilter invoke");
         return invoker.invoke(invocation);
     }
