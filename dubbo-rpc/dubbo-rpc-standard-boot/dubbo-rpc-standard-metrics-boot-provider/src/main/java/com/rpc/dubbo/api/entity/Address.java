cb4d80abd7f403b4619ca3cac69b05971b4520d1
xl-9527 <1824670705@qq.com>
2025-01-21 16:23:30 +0800

    feat(provider): 添加 Address 服务实现并优化配置
    - 在 AddressServiceImpl 中实现 queryList 方法，返回随机地址信息
    - 在 Address 类中添加 Lombok 注解，简化构造器和 getter/setter 代码- 更新 application.yml 配置，使用 protobuf 序列化方式- 移除不必要的配置项，简化配置文件结构

--- dubbo-rpc/dubbo-rpc-standard-boot/dubbo-rpc-standard-metrics-boot-provider/src/main/java/com/rpc/dubbo/api/entity/Address.java
+++ dubbo-rpc/dubbo-rpc-standard-boot/dubbo-rpc-standard-metrics-boot-provider/src/main/java/com/rpc/dubbo/api/entity/Address.java
@@ -1,11 +1,19 @@
 package com.rpc.dubbo.api.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * @author xl-9527
  * @since 2025/1/20
  **/
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Address {
+
+    private int addressId;
+
+    private String addressName;
 }
--- dubbo-rpc/dubbo-rpc-standard-boot/dubbo-rpc-standard-metrics-boot-provider/src/main/java/com/rpc/dubbo/service/AddressServiceImpl.java
+++ dubbo-rpc/dubbo-rpc-standard-boot/dubbo-rpc-standard-metrics-boot-provider/src/main/java/com/rpc/dubbo/service/AddressServiceImpl.java
@@ -3,18 +3,29 @@ package com.rpc.dubbo.service;
 import com.rpc.dubbo.api.entity.Address;
 import com.rpc.dubbo.api.service.AddressService;
 import org.apache.dubbo.config.annotation.DubboService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Random;
 
 /**
  * @author xl-9527
  * @since 2025/1/20
  **/
+@Service
 @DubboService(protocol = "triple")
 public class AddressServiceImpl implements AddressService {
 
+    private static final Logger log = LoggerFactory.getLogger(AddressServiceImpl.class);
+
+    private final Random random = new Random();
+
     @Override
     public List<Address> queryList() {
-        return List.of();
+        log.info("queryList -> {}", LocalDateTime.now().getNano());
+        return List.of(new Address(random.nextInt(), "xl-9527"));
     }
 }
